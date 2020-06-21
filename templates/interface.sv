9f162cedfcb8499f2ff3dcd505a0e4f0b2beca11
Psichico <jaimilpatel30@gmail.com>
2020-06-21 16:47:46 -0700

    added interface template

---
+++ templates/interface.sv
@@ -0,0 +1,13 @@
+/*========================================================
+File name:      interface.sv
+Description:    template
+Author:         Jaimil Patel
+Date created:   18 June 2020
+=========================================================*/
+
+interface interface_template(input logic clock);
+
+    //logic [7:0] a;
+    //logic [7:0] b;
+
+endinterface: interface_template
